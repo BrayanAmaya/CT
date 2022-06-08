7c8a17e8e0063cbc30c9599eae854f303467aa49
BryanAmaya <brayanantonio816@gmail.com>
2022-06-08 09:36:51 -0600

    update

--- app/Views/admin/incidencias.php
+++ app/Views/admin/incidencias.php
@@ -4,8 +4,7 @@
 Incidencias
 <?=$this->endSection()?>
 <?=$this->section('css')?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css
-">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 <link rel="stylesheet" href="/assets/css/style_admin.css">
 <?=$this->endSection()?>
 <?=$this->section('content')?>
@@ -16,4 +15,123 @@ Incidencias
     </div>
 </article>
 <?php endif; ?>
+
+<div class="container">
+  <div class="row">
+      
+  <div class="col-3">
+    <div class="card">
+    <div class="card" style="width: 22rem;">
+  <img src="https://bulma.io/images/placeholders/1280x960.png" class="card-img-top" alt="...">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      <br><br>
+      <a href="#" class="btn btn-primary">Resolver</a>  
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+
+
+    <div class="col-3">
+    <div class="card">
+    <div class="card" style="width: 22rem;">
+  <img src="https://bulma.io/images/placeholders/1280x960.png" class="card-img-top" alt="...">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      <br><br>
+      <a href="#" class="btn btn-primary">Resolver</a>  
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+
+    <div class="col-3">
+    <div class="card">
+    <div class="card" style="width: 22rem;">
+  <img src="https://bulma.io/images/placeholders/1280x960.png" class="card-img-top" alt="...">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      <br><br>
+      <a href="#" class="btn btn-primary">Resolver</a>  
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+
+
+    <div class="col-3">
+    <div class="card">
+    <div class="card" style="width: 22rem;">
+  <img src="https://bulma.io/images/placeholders/1280x960.png" class="card-img-top" alt="...">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      <br><br>
+      <a href="#" class="btn btn-primary">Resolver</a>  
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
 <?=$this->endSection()?>
